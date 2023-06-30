3519ca25ef321186a48bbdbced90cb03af9b0d59
Evelyn201 <72232806+Evelyn201@users.noreply.github.com>
2023-06-30 12:42:06 -0600

    Add files via upload

---
+++ mensajes_app/src/main/java/com/platzi/mensajes_app/Inicio.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platzi.mensajes_app;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Evely
+ */
+public class Inicio {
+     public static void main (String[] args){
+         
+         Scanner sc = new Scanner (System.in);
+         int opcion=0;
+         do{
+             System.out.println(":::::::::::::::::::::::::");
+             System.out.println("Aplicación de mensajes");
+             System.out.println("1. crear mensajes");
+             System.out.println("2. listar mensajes");
+             System.out.println("3. editar mensaje");
+             System.out.println("4. eliminar mensaje");
+             System.out.println("5. salir");
+             opcion = sc.nextInt();
+             
+             switch(opcion){
+                 case 1:
+                     mensajesService.crearMensaje();
+                     break;
+                 case 2:
+                     mensajesService.listarMensajes();
+                     break;
+                 case 3:
+                     mensajesService.borrarMensaje();
+                     break;
+                 case 4:
+                     mensajesService.editarMensaje();                     break;
+                 default:
+                     break;
+                     
+             }
+         }while(opcion != 5);
+     }
+}
